231b8c08b4d0dca5447b6011750c6c552f29c717
Rika Voice <skyvory@hotmail.com>
2017-05-02 14:37:30 +0800

    Unfinished link supercharge

--- link.php
+++ link.php
@@ -1,12 +1,22 @@
 <html>
-  <head>
-    <meta charset="UTF-8">
-  </head>
-  <body>
+
+<head>
+  <meta charset="UTF-8">
+</head>
+
+<body>
   <a href="index.php">BACK TO INDEX</a><br><br>
 
+  <!--<a href="#" id="superurl"><button onclick="supercharge()">SUPERCHARGE</button></a><br><br><br><br>-->
+  <script>
+    function supercharge() {
+      var el = document.getElementById('superurl');
+      console.log(el.href);
+    }
+  </script>
 
-<?php
+
+  <?php
 $txtfile = $_GET['txt'];
 // $filename = 'konkon.txt';
 $lines = file($txtfile, FILE_IGNORE_NEW_LINES);
@@ -14,11 +24,14 @@ $lines = file($txtfile, FILE_IGNORE_NEW_LINES);
 
 foreach($lines as $line) {
   ?>
-  <a href="https://google.com/search?q=<?= $line ?>&tbm=isch" target="_blank"><?=$line?></a><br>
-  <?php
+    <a href="https://google.com/search?q=<?= $line ?>&tbm=isch" target="_blank" class="tabable" onclick="clack()">
+      <?=$line?>
+    </a><br>
+    <?php
 }
 
 ?>
 
 </body>
+
 </html>
